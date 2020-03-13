25c64639d4ad9d5ddb06f57babe5d5c5bf65189b
Mateusz Baran <mateuszbaran89@gmail.com>
2020-03-13 16:00:58 +0100

    removing two unnecessary methods

--- src/DecoratorManifold.jl
+++ src/DecoratorManifold.jl
@@ -481,23 +481,10 @@ end
     kwargs...,
 )
 
-@decorator_transparent_signature check_manifold_point(
-    M::AbstractDecoratorManifold,
-    p::MPoint;
-    kwargs...,
-)
-
 @decorator_transparent_signature check_tangent_vector(
     M::AbstractDecoratorManifold,
     p,
     X;
-    kwargs...,
-)
-
-@decorator_transparent_signature check_tangent_vector(
-    M::AbstractDecoratorManifold,
-    p::MPoint,
-    X::TVector;
     kwargs...,
 )
 
