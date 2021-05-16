50b5129883e09d196d65c3cc72fd342f57aac7dd
Ronny Bergmann <git@ronnybergmann.net>
2021-05-16 23:57:35 +0200

    remove the actual implementation since it requires a dependency on Requires.jl.

--- src/retractions.jl
+++ src/retractions.jl
@@ -188,40 +188,6 @@ function inverse_retract(M::Manifold, p, q, method::AbstractInverseRetractionMet
     return X
 end
 
-
-@doc raw"""
-    inverse_retract(M, p, q method::NLsolveInverseRetraction; kwargs...)
-
-Approximate the inverse of the retraction specified by `method.retraction` from `p` with
-respect to `q` on the [`Manifold`](@ref) `M` using NLsolve. This inverse retraction is
-not guaranteed to succeed and probably will not unless `q` is close to `p` and the initial
-guess `X0` is close.
-
-If the solver fails to converge, an [`OutOfInjectivityRadiusError`](@ref) is raised.
-See [`NLsolveInverseRetraction`](@ref) for configurable parameters.
-"""
-inverse_retract(::Manifold, p, q, ::NLsolveInverseRetraction; kwargs...)
-
-function inverse_retract!(M::Manifold, X, p, q, method::NLsolveInverseRetraction; kwargs...)
-    X0 = method.X0 === nothing ? zero_tangent_vector(M, p) : method.X0
-    res = _inverse_retract_nlsolve(
-        M,
-        p,
-        q,
-        method.retraction,
-        X0,
-        method.project_tangent,
-        method.project_point,
-        method.nlsolve_kwargs;
-        kwargs...,
-    )
-    if !res.f_converged
-        @debug res
-        throw(OutOfInjectivityRadiusError())
-    end
-    return copyto!(X, res.zero)
-end
-
 @doc raw"""
     retract(M::Manifold, p, X)
     retract(M::Manifold, p, X, t::Real=1)
