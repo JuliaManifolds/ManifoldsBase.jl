8e456f2b2100a7e3f230999edfe69eed925e8d71
Mateusz Baran <mateuszbaran89@gmail.com>
2021-05-20 11:52:57 +0200

    improvements around FVector

--- src/vector_spaces.jl
+++ src/vector_spaces.jl
@@ -9,6 +9,39 @@ in which the coordinates are given.
 Conversion between `FVector` representation and the default representation of an object
 (for example a tangent vector) for a manifold should be done using [`get_coordinates`](@ref)
 and [`get_vector`](@ref).
+
+# Examples
+
+```julia-repl
+julia> using Manifolds
+
+julia> M = Sphere(2)
+Sphere(2, ℝ)
+
+julia> p = [1.0, 0.0, 0.0]
+3-element Vector{Float64}:
+ 1.0
+ 0.0
+ 0.0
+
+julia> X = [0.0, 2.0, -1.0]
+3-element Vector{Float64}:
+  0.0
+  2.0
+ -1.0
+
+julia> B = DefaultOrthonormalBasis()
+DefaultOrthonormalBasis(ℝ)
+
+julia> fX = TFVector(get_coordinates(M, p, X, B), B)
+TFVector([2.0, -1.0], DefaultOrthonormalBasis(ℝ))
+
+julia> X_back = get_vector(M, p, fX.data, fX.basis)
+3-element Vector{Float64}:
+ -0.0
+  2.0
+ -1.0
+```
 """
 struct FVector{TType<:VectorSpaceType,TData,TBasis<:AbstractBasis}
     type::TType
@@ -24,6 +57,13 @@ function TFVector(data, basis::AbstractBasis)
 end
 function CoTFVector(data, basis::AbstractBasis)
     return CoTFVector{typeof(data),typeof(basis)}(CotangentSpace, data, basis)
+end
+
+function Base.show(io::IO, fX::TFVector)
+    print(io, "TFVector(", fX.data, ", ", fX.basis, ")")
+end
+function Base.show(io::IO, fX::CoTFVector)
+    print(io, "CoTFVector(", fX.data, ", ", fX.basis, ")")
 end
 
 
