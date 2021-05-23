3f864a7d5911fe0286ece824c84d594c0f20cf91
Ronny Bergmann <git@ronnybergmann.net>
2021-05-23 12:11:50 +0200

    Apply suggestions from code review
    Co-authored-by: Mateusz Baran <mateuszbaran89@gmail.com>

--- src/exp_log_geo.jl
+++ src/exp_log_geo.jl
@@ -6,9 +6,9 @@ Compute the exponential map of tangent vector `X`, optionally scaled by `t`,  at
 from the manifold [`AbstractManifold`](@ref) `M`, i.e.
 
 ```math
-\exp_pX = γ_{p,X}(1),
+\exp_p X = γ_{p,X}(1),
 ```
-where ``γ_{p,X}`` is the unique geodesic starting in ``γ(0)=p`` and ``\dot γ(0) = X``.
+where ``γ_{p,X}`` is the unique geodesic starting in ``γ(0)=p`` such that ``\dot γ(0) = X``.
 
 See also [`shortest_geodesic`](@ref), [`retract`](@ref).
 """
