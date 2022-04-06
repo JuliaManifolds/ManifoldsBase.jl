7396d8a457ac21cc517e0b1eb20eb55976c8f0aa
Mateusz Baran <mateuszbaran89@gmail.com>
2022-04-06 19:02:39 +0200

    Resolve some basis-related ambiguities

--- src/point_vector_fallbacks.jl
+++ src/point_vector_fallbacks.jl
@@ -177,22 +177,34 @@ macro default_manifold_fallbacks(TM, TP, TV, pfield::Symbol, vfield::Symbol)
         cm = Symbol("get_coordinates_$(f_postfix)!")
         va = Symbol("get_vector_$(f_postfix)")
         vm = Symbol("get_vector_$(f_postfix)!")
-        push!(block.args, quote
-            function ManifoldsBase.$ca(M::$TM, p::$TP, X::$TV, B)
-                return ManifoldsBase.$ca(M, p.$pfield, X.$vfield, B)
-            end
-            function ManifoldsBase.$cm(M::$TM, Y, p::$TP, X::$TV, B)
-                ManifoldsBase.$cm(M, Y, p.$pfield, X.$vfield, B)
-                return Y
-            end
-            function ManifoldsBase.$va(M::$TM, p::$TP, X, B)
-                return $TV(ManifoldsBase.$va(M, p.$pfield, X, B))
-            end
-            function ManifoldsBase.$vm(M::$TM, Y::$TV, p::$TP, X, B)
-                ManifoldsBase.$vm(M, Y.$vfield, p.$pfield, X, B)
-                return Y
-            end
-        end)
+        B_type = if f_postfix in [:default, :orthogonal, :orthonormal, :vee]
+            :AbstractNumbers
+        elseif f_postfix === :cached
+            :CachedBasis
+        elseif f_postfix === :diagonalizing
+            :DiagonalizingOrthonormalBasis
+        else
+            :Any
+        end
+        push!(
+            block.args,
+            quote
+                function ManifoldsBase.$ca(M::$TM, p::$TP, X::$TV, B::$B_type)
+                    return ManifoldsBase.$ca(M, p.$pfield, X.$vfield, B)
+                end
+                function ManifoldsBase.$cm(M::$TM, Y, p::$TP, X::$TV, B::$B_type)
+                    ManifoldsBase.$cm(M, Y, p.$pfield, X.$vfield, B)
+                    return Y
+                end
+                function ManifoldsBase.$va(M::$TM, p::$TP, X, B::$B_type)
+                    return $TV(ManifoldsBase.$va(M, p.$pfield, X, B))
+                end
+                function ManifoldsBase.$vm(M::$TM, Y::$TV, p::$TP, X, B::$B_type)
+                    ManifoldsBase.$vm(M, Y.$vfield, p.$pfield, X, B)
+                    return Y
+                end
+            end,
+        )
     end
     # TODO  forward retraction / inverse_retraction
     for f_postfix in [:polar, :project, :qr, :softmax]
