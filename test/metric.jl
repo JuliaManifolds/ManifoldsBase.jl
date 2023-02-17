b7b69a9d42e9225eee64a02f6f33c9995416dc94
Ronny Bergmann <git@ronnybergmann.net>
2023-02-17 16:26:50 +0100

    Remove a line from the tests.

--- test/metric.jl
+++ test/metric.jl
@@ -1,6 +1,5 @@
 using Test
 using ManifoldsBase
-import ManifoldsBase: inner, change_representer, change_metric
 
 @testset "Metrics" begin
     @test ManifoldsBase.EuclideanMetric() isa ManifoldsBase.AbstractMetric
