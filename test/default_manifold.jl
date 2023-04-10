30686192b7d59d36284a5e171b00e98b5e597d51
Mateusz Baran <mateuszbaran89@gmail.com>
2023-04-10 22:00:46 +0200

    Allow copying scalars (#154)
    * Allow copying scalars
    
    * minor change
    
    * missing test

--- test/default_manifold.jl
+++ test/default_manifold.jl
@@ -857,4 +857,12 @@ Base.size(x::MatrixVectorTransport) = (size(x.m, 2),)
     @testset "performance" begin
         @allocated isapprox(M, SA[1, 2], SA[3, 4]) == 0
     end
+
+    @testset "scalars" begin
+        M = DefaultManifold()
+        p = 1.0
+        X = 2.0
+        @test copy(M, p) === p
+        @test copy(M, p, X) === X
+    end
 end
