4bd079168a8e49bdc6b7bd8b3e3f7f1ee054029a
Ronny Bergmann <git@ronnybergmann.net>
2023-08-17 15:23:05 +0200

    Update src/ManifoldsBase.jl
    Co-authored-by: Mateusz Baran <mateuszbaran89@gmail.com>

--- src/ManifoldsBase.jl
+++ src/ManifoldsBase.jl
@@ -922,12 +922,12 @@ Compute the Weingarten map ``\mathcal W_p\colon T_p\mathcal M × N_p\mathcal M \
 where ``N_p\mathcal M`` is the orthogonal complement of the tangent space ``T_p\mathcal M``
 of the embedded submanifold ``\mathcal M``, where we denote the embedding by ``\mathcal E``.
 
-One interpretation can also be given by looking at the differential of the [`project`](@ref)ion
-``\operatorname{proj}_{T_p\mathcal M}\colon \mathcal E \to T_p\mathcal M`` with respect to the base point ``p``,
+The Weingarten map can be defined by restricting the differential of the orthogonal [`project`](@ref)ion
+``\operatorname{proj}_{T_p\mathcal M}\colon T_p \mathcal E \to T_p\mathcal M`` with respect to the base point ``p``,
 i.e. defining
 ```math
 \mathcal P_X \coloneqq D_p\operatorname{proj}_{T_p\mathcal M}(Y)[X],
-\qquad Y \in \mathcal E, X \in T_p\mathcal M,
+\qquad Y \in T_p \mathcal E, X \in T_p\mathcal M,
 ```
 the Weingarten map can be written as ``\mathcal W_p(X,V) = \mathcal P_X(V)``.
 
