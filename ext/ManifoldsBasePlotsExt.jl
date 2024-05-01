7c53b55ff6e1eb157dce7450f7519608f2ddeab7
Ronny Bergmann <git@ronnybergmann.net>
2024-05-01 16:27:14 +0200

    Moing Statistics to a weak dependency caused an error (I do not understand) with Printf within the Plots Extensin, so maybe this fixes it?
    This is all magic land.

--- ext/ManifoldsBasePlotsExt.jl
+++ ext/ManifoldsBasePlotsExt.jl
@@ -1,14 +1,16 @@
 module ManifoldsBasePlotsExt
 
 if isdefined(Base, :get_extension)
-    using Plots
     using ManifoldsBase
+    using Plots
+    using Printf: @sprintf
     import ManifoldsBase: plot_slope
 else
     # imports need to be relative for Requires.jl-based workflows:
     # https://github.com/JuliaArrays/ArrayInterface.jl/pull/387
-    using ..Plots
     using ..ManifoldsBase
+    using ..Plots
+    using ..Printf: @sprintf
     import ..ManifoldsBase: plot_slope
 end
 
