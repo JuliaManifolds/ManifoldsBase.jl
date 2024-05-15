38539ad98adb462a0a438f26d35c2b093474374e
Ronny Bergmann <git@ronnybergmann.net>
2024-05-15 14:44:17 +0200

    adapt tests.

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -5,6 +5,7 @@ using Aqua, ManifoldsBase, Test
         ManifoldsBase;
         ambiguities = (exclude = [
             allocate, # has many possible call patterns that are not supported and ambiguous
+            fill, # points will probably not be Integer or a range, so this should be fine.
             getindex, # ambiguous call patterns are not supported
             setindex!, # ambiguous call patterns are not supported
         ], broken = false),
