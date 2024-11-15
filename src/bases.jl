621b5a76be7cbc4765595f739d924d2e5f554c55
Mateusz Baran <mateuszbaran89@gmail.com>
2024-11-15 14:36:52 +0100

    expand docstring

--- src/bases.jl
+++ src/bases.jl
@@ -842,6 +842,8 @@ end
     get_vectors(M::AbstractManifold, p, B::AbstractBasis=get_basis(M, p, DefaultOrthonormalBasis()))
 
 Get the basis vectors of basis `B` of the tangent space at point `p`.
+The function may or may not work if passed a basis other than a [`CachedBasis`](@ref).
+A `CachedBasis` can be obtained by calling [`get_basis`](@ref).
 """
 function get_vectors(M::AbstractManifold, p, B::AbstractBasis)
     return _get_vectors(M, p, B)
