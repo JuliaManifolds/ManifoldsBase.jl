8995e7ebbb15988a37d8ee7a5c428c07ef8c5dd4
Mateusz Baran <mateuszbaran89@gmail.com>
2025-01-05 17:29:22 +0100

    post old tutorial as a discussion

--- docs/make.jl
+++ docs/make.jl
@@ -71,7 +71,7 @@ makedocs(;
     pages = [
         "Home" => "index.md",
         "How to define a manifold" => "tutorials/implement-a-manifold.md",
-        "What are manifolds?" => "tutorials/what-are-manifolds.md",
+        "What are manifolds?" => "tutorials/what-are-manifolds2.md",
         "Design principles" => "design.md",
         "An abstract manifold" => "types.md",
         "Functions on manifolds" => [
