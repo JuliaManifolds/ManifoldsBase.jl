ff79e1aced4396a30d69a297ac8e5597778f92ad
Mateusz Baran <mateuszbaran89@gmail.com>
2025-04-08 16:11:56 +0200

    Relax VectorSpaceFiber type

--- src/VectorFiber.jl
+++ src/VectorFiber.jl
@@ -1,12 +1,12 @@
 
 """
     VectorSpaceFiber{𝔽,M,TSpaceType} = Fiber{𝔽,TSpaceType,M}
-        where {𝔽,M<:AbstractManifold{𝔽},TSpaceType<:VectorSpaceType}
+        where {𝔽,M<:AbstractManifold,TSpaceType<:VectorSpaceType}
 
 Alias for a [`Fiber`](@ref) when the fiber is a vector space.
 """
 const VectorSpaceFiber{𝔽,M,TSpaceType} =
-    Fiber{𝔽,TSpaceType,M} where {𝔽,M<:AbstractManifold{𝔽},TSpaceType<:VectorSpaceType}
+    Fiber{𝔽,TSpaceType,M} where {𝔽,M<:AbstractManifold,TSpaceType<:VectorSpaceType}
 
 LinearAlgebra.norm(M::VectorSpaceFiber, X) = norm(M.manifold, M.point, X)
 # disambiguation
