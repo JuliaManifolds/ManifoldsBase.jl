523fc8a5c2ae2cfc96c624bf9bc948501ca4ec50
Mateusz Baran <mateuszbaran89@gmail.com>
2025-10-23 17:07:24 +0200

    improve text

--- src/ManifoldsBase.jl
+++ src/ManifoldsBase.jl
@@ -688,16 +688,16 @@ injectivity_radius_exp(M::AbstractManifold) = injectivity_radius(M)
 Compute the inner product of tangent vectors `X` and `Y` at point `p` from the
 [`AbstractManifold`](@ref) `M`. The result type varies by the exact type of `M`:
 
-* On a [Riemannian manifold](https://en.wikipedia.org/wiki/Riemannian_manifold)
-  the result is a real number.
-* On an [almost complex manifold](https://en.wikipedia.org/wiki/Almost_complex_manifold)
-  the result is a complex number.
-* On an [almost quaternionic manifold](https://en.wikipedia.org/wiki/Quaternionic_manifold#Almost_quaternionic_structure)
-  the result is a quaternion.
-* On a [CR manifold](https://en.wikipedia.org/wiki/CR_manifold) the result may be either
-  real or complex, depending on the particular manifold.
+1. On a [Riemannian manifold](https://en.wikipedia.org/wiki/Riemannian_manifold)
+   the result is a real number.
+2. On an [almost complex manifold](https://en.wikipedia.org/wiki/Almost_complex_manifold)
+   the result is a complex number.
+3. On an [almost quaternionic manifold](https://en.wikipedia.org/wiki/Quaternionic_manifold#Almost_quaternionic_structure)
+   the result is a quaternion.
+4. On a [CR manifold](https://en.wikipedia.org/wiki/CR_manifold) the result may be either
+   real or complex, depending on the particular manifold.
 
-In each case, the Riemannian inner product can be obtained by taking the real part.
+In cases 2, 3 and 4 the Riemannian inner product can be obtained by taking the real part.
 """
 inner(M::AbstractManifold, p, X, Y)
 
